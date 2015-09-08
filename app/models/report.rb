ef3533030910e6dc21cf1070beb5bdb15033c44c
Thomas Hutterer <thutterer@suse.de>
2015-09-08 22:38:25 +0200

    calculate time balance in Report

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,12 +4,13 @@ class Report < ActiveRecord::Base
   friendly_id :title, use: :slugged
 
   # Callbacks
-  before_validation { |report| report.title = report.workday.strftime('%F')}
+  before_save { |report| report.title = report.workday.strftime('%F')}
   before_save { MarkdownWriter.update_html(self) }
+  # TODO break_duration??? what the fuck? kill!!
   before_save { |report| report.break_duration = "00:00" unless report.break_duration}
 
   # Validations
-  validates :title, presence: true
+  validates :workday, presence: true
   validates :title, uniqueness: { scope: [:user] }
 
   ### not needed anymore but as a reminder how to write custom validations like this :)
@@ -24,15 +25,35 @@ class Report < ActiveRecord::Base
   # Relations
   belongs_to :user
 
+
   def self.by_month(month, year = Time.now.year)
     month = format('%02d', month)
     where(workday: "#{year}-#{month}-01".."#{year}-#{month}-#{Time.days_in_month(month.to_i, year)}")
   end
 
+  def self.where_worked
+    where("worked_from and worked_until")
+  end
+
+
+  def self.actual_workhours
+    self.where_worked.map(&:worked_seconds).sum / 3600
+  end
+
+  def self.expected_workhours
+    self.where_worked.count * 8 # TODO make 8h a configurable
+  end
+
+  def self.balance
+    self.actual_workhours - self.expected_workhours
+  end
+
+
   def worked_seconds
     return 0 if worked_from.nil? or break_duration.nil? or worked_until.nil?
     worked_until - worked_from - break_duration.seconds_since_midnight
   end
+
 
   # Scopes
   scope :published, lambda {
